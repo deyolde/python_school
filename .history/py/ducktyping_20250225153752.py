4afd5015bdb82fafb0517da18aa9ab1695a8f25d
David Yolde <deyolde@gmail.com>
2025-02-28 16:03:15 -0300

    update con ducktiping
    update con ducktiping

--- .history/py/ducktyping_20250225153752.py
+++
@@ -1,7 +0,0 @@
-El Duck Typing en Python es un concepto de la programación dinámica que se basa en e  comportamiento de un objeto en lugar de su tipo explícito.
-
-👉 Explicación sencilla:
-Si un objeto se comporta como un pato (camina como un pato y hace "cuac" como un pato), entonces Python lo trata como un pato, sin importar si realmente es un pato. 🦆
-
-En términos de programación:
-"No me importa de qué tipo es el objeto, solo me importa si puede hacer lo que necesito."
